5271a62dd221ebada451a7beb35ee41cbae4ed5c
Poonam Verma <poonam.verma02@nagarro.com>
2024-05-31 17:23:57 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,5 @@
+ 
+const elem = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child1" }, [React.createElement("h1", {}, "heading1"), React.createElement("h1", {}, "heading2")]),
+React.createElement("div", { id: "child2" }, [React.createElement("h1", {}, "heading1"), React.createElement("h1", {}, "heading2")])]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(elem);
